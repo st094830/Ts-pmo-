7877c3f8ec85e4cc65a5437eda95cfab35d5aad4
st094830 <ivanshutemov2005@gmail.com>
2025-04-09 23:31:38 +0300

    Lab3 first calculations, sensitivity done most likely

---
+++ Lab3/Lab3/Header.h
@@ -0,0 +1,68 @@
+#pragma once
+#pragma once
+#ifndef DATAPROC_H
+#define DATAPROC_H
+
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+class DataProcessor {
+public:
+    DataProcessor(const std::string& inputFileName);
+    ~DataProcessor() {};
+    void readData();
+    void calculateMean();
+    void calculateDeviations();
+    void calculateAbsoluteErrors();
+    void calculateStandardDeviation();
+    void calculateMeanError();
+    double getMaxDelta() const;
+    double getStandardDeviation() const;
+    double getMeanError() const;
+    const std::vector<double>& getDeviations() const;
+    const std::vector<double>& getSquaredDeviations() const;
+    double getMean() const;
+    std::vector<double> getSensitivity();
+
+private:
+    std::string inputFileName;
+    std::vector<double> data;
+    double mean;
+    std::vector<double> deviations;
+    std::vector<double> squaredDeviations;
+    std::vector<double> deltas;
+    double maxDelta;
+    double standardDeviation;
+    double meanError;
+    std::vector<double> sensitivity;
+};
+
+int main(int argc, char* argv[]) 
+{
+    std::ofstream outputFile("Sensitivity.txt");
+
+    DataProcessor Vert ("Vert.txt");
+    DataProcessor Horus ("Horus.txt");
+    DataProcessor Maxx ("Maxx.txt");
+    Vert.readData();
+    Horus.readData();
+    Maxx.readData();
+    std::vector<double> Senss = Vert.getSensitivity();
+    for (int i = 0; i < 5; i++) {
+        outputFile << Senss[i] << "\n";
+    }
+    outputFile << "\n ______ \n";
+    Senss = Horus.getSensitivity();
+    for (int i = 0; i < 5; i++) {
+        outputFile << Senss[i] << "\n";
+    }
+    outputFile << "\n ______ \n";
+    Senss = Maxx.getSensitivity();
+    for (int i = 0; i < 5; i++) {
+        outputFile << Senss[i] << "\n";
+    }
+}
+#endif
